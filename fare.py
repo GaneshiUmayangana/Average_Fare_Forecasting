cbffdbeb6f18a8bd2ba2e08db05b315c0a317364
Ganeshi Umayangana <158500709+GaneshiUmayangana@users.noreply.github.com>
2025-01-20 12:03:16 +0530

    Update fare.py

--- fare.py
+++ fare.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-#import plotly
-import plotly.graph_objects as go
+import plotly.express as px
+#import plotly.graph_objects as go
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 import matplotlib.pyplot as plt
